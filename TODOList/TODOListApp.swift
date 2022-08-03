fb0a90c545b0f5c36d6b8c42d4744d9feceab286
장효원 <hyo961015@naver.com>
2022-08-03 20:19:29 +0900

    init

---
+++ TODOList/TODOListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOListApp.swift
+//  TODOList
+//
+//  Created by 장효원 on 2022/08/03.
+//
+
+import SwiftUI
+
+@main
+struct TODOListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
